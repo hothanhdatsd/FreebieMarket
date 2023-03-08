e7b2e1af0704fa777aeb42fd43c75696535250b9
ThanhDattt <93121654+hothanhdatsd@users.noreply.github.com>
2023-03-08 21:06:23 +0700

    first commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+  importProduct,
+} from "../controller/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/import").post(protect, admin, importProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
+
+export default router;
